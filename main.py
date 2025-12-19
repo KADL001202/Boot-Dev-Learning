fb92b83e59155d2f2654c6724d01d0b76c2b7f7c
Kemuel De Leon <122828000+KADL001202@users.noreply.github.com>
2025-12-19 14:51:28 +0800

    CH6 Polymorphism Level 11 Overriding Built-In Methods

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-class Sword:
-    def __init__(self, sword_type):
-        self.sword_type = sword_type
-    
-    def __add__(self, other):
-        if self.sword_type == "bronze" and other.sword_type == "bronze":
-            return Sword("iron")
-        elif self.sword_type == "iron" and other.sword_type == "iron":
-            return Sword("steel")
-        else:
-            raise Exception("cannot craft")
+class Dragon:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def __str__(self):
+        return f"I am {self.name}, the {self.color} dragon"
